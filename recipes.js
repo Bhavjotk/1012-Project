c883a788fbeda39425a99aab487f43b85ffba096
Bhavjotk <114354339+Bhavjotk@users.noreply.github.com>
2024-11-17 19:21:06 -0500

    Add files via upload

--- recipes.js
+++ recipes.js
@@ -26,3 +26,30 @@ registerBtn.addEventListener('click', ()=> {
 closeScreen.addEventListener('click', ()=> {
     loginScreen.classList.remove('active');
 })
+
+// Nav Bar
+// const navItems = document.querySelectorAll('.topnav');
+// navItems.forEach(item => {
+//     item.addEventListener('mouseover', function(e) {
+//     //   e.target.style.backgroundColor = '#ff0000';
+//     });
+  
+//     item.addEventListener('click', function(e) {
+//     //   e.target.style.color = '#00ff00';
+//     });
+//   });
+
+
+
+  document.querySelectorAll('.dropdown').forEach(dropdown => {
+    dropdown.addEventListener('mouseover', () => {
+        dropdown.querySelector('.dropdown-content').style.display = 'block';
+    });
+
+    dropdown.addEventListener('mouseout', () => {
+        dropdown.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
+
+
+  
