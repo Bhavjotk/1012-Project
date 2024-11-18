d692c30a0c185826180f387d3dda85a839ef689b
Bhavjotk <114354339+Bhavjotk@users.noreply.github.com>
2024-11-18 18:15:33 -0500

    Add files via upload

--- recipes.js
+++ recipes.js
@@ -41,15 +41,6 @@ closeScreen.addEventListener('click', ()=> {
 
 
 
-  document.querySelectorAll('.dropdown').forEach(dropdown => {
-    dropdown.addEventListener('mouseover', () => {
-        dropdown.querySelector('.dropdown-content').style.display = 'block';
-    });
-
-    dropdown.addEventListener('mouseout', () => {
-        dropdown.querySelector('.dropdown-content').style.display = 'none';
-    });
-});
-
+  
 
   
