c00c7d2f7142addcb4ea578066995494f7a91408
Bhavjotk <114354339+Bhavjotk@users.noreply.github.com>
2024-11-23 23:16:24 -0500

    Add files via upload

---
+++ newrecipes-page.js
@@ -0,0 +1,46 @@
+/* Final Project */
+
+function login() {
+    x.style.left = "10px";
+    y.style.right = "-550px";
+}
+
+function register() {
+    x.style.left = "-550px";
+    y.style.right = "-10px";
+}
+
+const loginScreen = document.querySelector('.account-form');
+const loginBtn = document.querySelector('.loginButton');
+const registerBtn = document.querySelector('.registerButton');
+const closeScreen = document.querySelector('.close-screen');
+
+loginBtn.addEventListener('click', ()=> {
+    loginScreen.classList.add('active');
+});
+
+registerBtn.addEventListener('click', ()=> {
+    loginScreen.classList.add('active');
+});
+
+closeScreen.addEventListener('click', ()=> {
+    loginScreen.classList.remove('active');
+})
+
+// Nav Bar
+// const navItems = document.querySelectorAll('.topnav');
+// navItems.forEach(item => {
+//     item.addEventListener('mouseover', function(e) {
+//     //   e.target.style.backgroundColor = '#ff0000';
+//     });
+  
+//     item.addEventListener('click', function(e) {
+//     //   e.target.style.color = '#00ff00';
+//     });
+//   });
+
+
+
+  
+
+  
