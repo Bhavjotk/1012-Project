930eece7a92bde09fb0e23210850e8b1106f985b
Bhavjotk <114354339+Bhavjotk@users.noreply.github.com>
2024-11-30 15:07:28 -0500

    Add files via upload

--- course.js
+++ course.js
@@ -1,1 +1,27 @@
 
+function login() {
+    x.style.left = "10px";
+    y.style.right = "-550px";
+}
+
+function register() {
+    x.style.left = "-550px";
+    y.style.right = "-10px";
+}
+
+const loginScreen = document.querySelector('.account-form');
+const loginBtn = document.querySelector('.loginButton');
+const registerBtn = document.querySelector('.registerButton');
+const closeScreen = document.querySelector('.close-screen');
+
+loginBtn.addEventListener('click', ()=> {
+    loginScreen.classList.add('active');
+});
+
+registerBtn.addEventListener('click', ()=> {
+    loginScreen.classList.add('active');
+});
+
+closeScreen.addEventListener('click', ()=> {
+    loginScreen.classList.remove('active');
+})
