7da195ef612b5b8a2172f5583812213f029930c9
Bhavjotk <114354339+Bhavjotk@users.noreply.github.com>
2024-12-02 16:47:04 -0500

    Add files via upload

--- server/main-server.js
+++ server/main-server.js
@@ -4,14 +4,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../client'));
 
 // Route
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/homepage.html');
+    res.sendFile('homepage.html', { root: __dirname + '/../client' });
 });
 
 // Listen on port
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
+    console.log(__dirname + '/../client/homepage.html');
   }); //server will need to listen ... all the time!!
