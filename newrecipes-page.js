532dff2174fbf21859fe5727df3b12b3866f457d
halanemu <halanemu@my.yorku.ca>
2024-12-02 12:33:58 -0500

    Update newrecipes-page.js

--- newrecipes-page.js
+++ newrecipes-page.js
@@ -1,4 +1,4 @@
-/* Final Project */
+/* New Recipes Page Login Page */
 
 function login() {
     x.style.left = "10px";
@@ -25,23 +25,105 @@ registerBtn.addEventListener('click', ()=> {
 
 closeScreen.addEventListener('click', ()=> {
     loginScreen.classList.remove('active');
+});
+
+// New Recipes Page
+const recipeList = document.getElementById('results');
+const loadContent = document.querySelector('.add-recipe');
+const recipeContent = document.querySelector('.recipe-content');
+const recipeCloseScreen = document.querySelector('.recipe-close');
+
+// Event Listeners
+recipeList.addEventListener('click', getRecipe);
+loadContent.addEventListener('click', addRecipes);
+recipeCloseScreen.addEventListener('click', ()=> {
+    recipeContent.parentElement.classList.remove('showRecipe');
+});
+
+let promises = [];
+
+for (let i = 1;i <= 5; i++) {
+    promises.push(fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(response => response.json()));
+};
+
+Promise.all(promises).then(data => {
+    for (let j = 0; j < data.length; j++){
+        let html = "";
+        if (data[j].meals){
+            data[j].meals.forEach(meal => {
+                html += `<div class="meal-item" data-id = "${meal.idMeal}">
+                            <div class="meal-img">
+                                <img src= "${meal.strMealThumb}" alt = "meal">
+                            </div>
+                            <div class="meal-name">
+                                <h3>${meal.strMeal}</h3>
+                                <a href="#" class="recipe-btn">Recipe Info</a>
+                            </div>
+                        </div>`
+            });
+        }
+        recipeList.innerHTML += html;
+    }
 })
 
-// Nav Bar
-// const navItems = document.querySelectorAll('.topnav');
-// navItems.forEach(item => {
-//     item.addEventListener('mouseover', function(e) {
-//     //   e.target.style.backgroundColor = '#ff0000';
-//     });
-  
-//     item.addEventListener('click', function(e) {
-//     //   e.target.style.color = '#00ff00';
-//     });
-//   });
+// Load more recipes
+function addRecipes(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('add-recipe')) {
+        let newPromises = [];
 
+        for (let i = 1;i <= 5; i++) {
+            newPromises.push(fetch('https://www.themealdb.com/api/json/v1/1/random.php').then(response => response.json()));
+        };
 
+        Promise.all(newPromises).then(data => {
+            for (let j = 0; j < data.length; j++){
+                let html = "";
+                if (data[j].meals){
+                    data[j].meals.forEach(meal => {
+                        html += `<div class="meal-item" data-id = "${meal.idMeal}">
+                                    <div class="meal-img">
+                                        <img src= "${meal.strMealThumb}" alt = "meal">
+                                    </div>
+                                    <div class="meal-name">
+                                        <h3>${meal.strMeal}</h3>
+                                        <a href="#" class="recipe-btn">Recipe Info</a>
+                                    </div>
+                                </div>`
+                    });
+                }
+                recipeList.innerHTML += html;
+            }
+        })
+    }
+}
 
-  
+// Get the contents of the recipe
+function getRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('recipe-btn')) {
+        let recipe = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipe.dataset.id}`).then(response => response.json())
+        .then(data => recipeModal(data.meals));
+    }
+}
 
-  
-// interactivityy
+// Make a recipe modal
+function recipeModal(meal) {
+    meal = meal[0];
+    let html = `<h2 class="recipe-title">${meal.strMeal}</h2>
+                <p class="recipe-category">${meal.strCategory}</p>
+                <div class="recipe-instruct">
+                    <h3>Instructions:</h3>
+                    <p>${meal.strInstructions}</p>
+                </div>
+                <div class="recipe-img">
+                    <img src= "${meal.strMealThumb}" alt="">
+                </div>
+                <div class="recipe-link">
+                    <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+                </div>`;
+
+    recipeContent.innerHTML = html;
+    recipeContent.parentElement.classList.add('showRecipe');
+}
